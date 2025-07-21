707894f649c8d54ea15c3c2bdf5a537c85b476c1
Jeremy Dumais <jeremydumais@hotmail.com>
2025-07-21 07:59:37 -0400

    feat: Add neoclip to the config

--- lua/jed/packer.lua
+++ lua/jed/packer.lua
@@ -110,6 +110,17 @@ return require('packer').startup(function(use)
 
     use('jose-elias-alvarez/null-ls.nvim')
     use('MunifTanjim/prettier.nvim')
+    use {
+        "AckslD/nvim-neoclip.lua",
+        requires = {
+            -- you'll need at least one of these
+            -- {'nvim-telescope/telescope.nvim'},
+            -- {'ibhagwan/fzf-lua'},
+        },
+        config = function()
+            require('neoclip').setup()
+        end,
+    }
     use('roxma/vim-tmux-clipboard')
     use('tmux-plugins/vim-tmux-focus-events')
 end)
